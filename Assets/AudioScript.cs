cd3b529527f2f8323e363ce32866f6099d6cebd3
ratan <ratan@rsdharra.com>
2020-06-21 21:17:16 +0530

    files added

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    AudioSource myAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+        myAudio.PlayDelayed(1.2f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ButtonClickedScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonClickedScript : MonoBehaviour
+{
+
+
+    public void ActionButtonPressed(){
+        Debug.Log("YAY!!!!!!!! Button got pressed!!!");
+        SceneManager.LoadScene("SampleScene");
+    }
+}
